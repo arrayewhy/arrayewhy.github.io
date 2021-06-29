8fa1638b69cbe0c48bb62ab365d12b1e32de50ac
Nomad <random90s@gmail.com>
2021-06-30 04:00:24 +0800

    Updates

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,21 @@
 $(function ()
 {
-	// Hover Effects
+	// Work Thumbnail
 
-	$(".hover").on ("mouseover", function ()
-	{
-		$(this).css ("opacity", "1");
-	});
-	$(".hover").on ("mouseout", function ()
-	{
-		$(this).css ("opacity", "0.4");
-	});
+	// $(".work-thumbnail").on ("click", function ()
+	// {
+	// 	$("#work-thumbnail-container").fadeOut ("fast");
+	// });
+
+	// Hover
+
+	$(".hover")
+	.on ("mouseover", function () { $(this).css ("opacity", "1"); })
+	.on ("mouseout", function () { $(this).css ("opacity", "0.4"); });
+
+	// Soft Hover
+
+	$(".soft-hover")
+	.on ("mouseover", function () { $(this).css ("opacity", "1"); })
+	.on ("mouseout", function () { $(this).css ("opacity", "0.2"); });
 });
