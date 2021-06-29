ae98ceb7cc8651f1a4ec38162ebd3525d9d9cf49
Nomad <random90s@gmail.com>
2021-06-29 19:02:09 +0800

    Update [Hover Effects]

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+$(function ()
+{
+	$(".hover").on ("mouseover", function ()
+	{
+		$(this).css ("opacity", "1");
+	});
+	$(".hover").on ("mouseout", function ()
+	{
+		$(this).css ("opacity", "0.4");
+	});
+});
