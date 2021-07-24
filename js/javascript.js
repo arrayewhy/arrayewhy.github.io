1ded3f7145b39ffae29982de57d504bdf08c11ae
Nomad <random90s@gmail.com>
2021-07-24 17:36:49 +0800

    Update [Sketches]

--- js/javascript.js
+++ js/javascript.js
@@ -24,4 +24,23 @@ $(function ()
 	$(".fade-out")
 	.on ("mouseover", function () { $(this).css ("opacity", "0.6"); })
 	.on ("mouseout", function () { $(this).css ("opacity", "1"); });
+
+	// Sketch Zoom
+
+	$(".sketchbook-thumbnail").on ("click", function ()
+	{
+		var imgUrl = "url(" + $(this).attr ("src") + ") no-repeat";
+		$("#sketch-zoom").css ("width", "100%");
+		$("#sketch-zoom").css ("height", "100%");
+		$("#sketch-zoom").css ("visibility", "visible");
+		$("#sketch-zoom").css ("background", imgUrl);
+		$("#sketch-zoom").css ("background-size", "auto 100%");
+	});
+
+	$("#sketch-zoom").on ("click", function ()
+	{
+		$(this).css ("visibility", "hidden");
+		$(this).css ("width", "0");
+		$(this).css ("height", "0");
+	});
 });
