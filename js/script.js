12bab1e668899f7621d7b1288ac72d68d78138c6
Sundrop <random90s@gmail.com>
2024-12-28 03:14:09 +0800

    Removed absolute positioning from Footer on Thoughts Page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 const htmlElement = document.querySelector('html');
 
+var anchors = document.getElementsByTagName("a");
+
 window.onload = function() {
 	htmlElement.onload = Show_Page();
-	// document.getElementsByTagName("html").onload = Show_Page(S);
+
+	for(var i = 0; i < anchors.length - 1; i++) {
+		anchors[i].onclick = function() { Hide_Page(); }
+	}
 }
 
 function Show_Page() {
 	htmlElement.style.opacity = "1";
+}
+
+function Hide_Page() {
+	htmlElement.style.transition = ".25s ease";
+	htmlElement.style.opacity = "0";
 }
