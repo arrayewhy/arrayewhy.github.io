558e54591daa6ca6f84cd921d69058679bc0b05f
Sundrop <random90s@gmail.com>
2024-12-28 03:16:43 +0800

    Disable Hide_Page()

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ var anchors = document.getElementsByTagName("a");
 window.onload = function() {
 	htmlElement.onload = Show_Page();
 
-	for(var i = 0; i < anchors.length - 1; i++) {
-		anchors[i].onclick = function() { Hide_Page(); }
-	}
+	// for(var i = 0; i < anchors.length - 1; i++) {
+	// 	anchors[i].onclick = function() { Hide_Page(); }
+	// }
 }
 
 function Show_Page() {
 	htmlElement.style.opacity = "1";
 }
 
-function Hide_Page() {
-	htmlElement.style.transition = ".25s ease";
-	htmlElement.style.opacity = "0";
-}
+// function Hide_Page() {
+// 	htmlElement.style.transition = ".25s ease";
+// 	htmlElement.style.opacity = "0";
+// }
