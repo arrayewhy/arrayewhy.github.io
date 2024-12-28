90fa567f816418decedb0e9419139fe27fabb55c
Sundrop <random90s@gmail.com>
2024-12-28 17:22:32 +0800

    Added an Imp to the Logo

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 const htmlElement = document.querySelector('html');
+var logoCont = null;
+
+var imp = null;
+var drifting = false;
 
 var anchors = document.getElementsByTagName("a");
 
 window.onload = function() {
 	htmlElement.onload = Show_Page();
 
-	// for(var i = 0; i < anchors.length - 1; i++) {
-	// 	anchors[i].onclick = function() { Hide_Page(); }
-	// }
+	logoCont = document.getElementById("logo-cont");
+
+	imp = document.getElementById("imp");
+	logoCont.onmouseenter = function() { Imp_Drift(); }
+}
+
+function Imp_Drift() {
+	if (drifting) {
+		return;
+	}
+	drifting = true;
+	imp.classList.add("DriftRightAnim");
+	setTimeout(function() {
+		imp.classList.remove("DriftRightAnim");
+		drifting = false;
+	}, 2000);
 }
 
 function Show_Page() {
 	htmlElement.style.opacity = "1";
 }
-
-// function Hide_Page() {
-// 	htmlElement.style.transition = ".25s ease";
-// 	htmlElement.style.opacity = "0";
-// }
