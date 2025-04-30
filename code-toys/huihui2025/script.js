b94cd6b3e548aabbb5a7d3f6ab7d4c8b41b2fd1d
Sundrop <random90s@gmail.com>
2025-05-01 04:53:06 +0800

    Add [huihui2025 - Audio]

--- code-toys/huihui2025/script.js
+++ code-toys/huihui2025/script.js
@@ -2,6 +2,9 @@
 // General
 var html = document.getElementsByTagName("html")[0];
 
+// Login
+var loginWrapper = document.getElementById("login-wrapper");
+
 // Icons
 var icons = document.getElementsByClassName("icon");
 var emailIcon = document.getElementById("email-icon");
@@ -18,15 +21,23 @@ var envelope = document.getElementById("envelope");
 var letterWrapper = document.getElementById("letter-wrapper");
 
 // Buttons
+var loginBtn = document.getElementById("login-btn");
 var backBtns = document.getElementsByClassName("back-btn");
 
 // Audio
 var soundBtns = document.getElementsByClassName("sound-btn")
 var pop = document.getElementById("pop");
 var popLoud = document.getElementById("pop-loud");
+var popPitchedDown = document.getElementById("pop-pitched-down");
+var forestAmbience = document.getElementById("forest-ambience");
+var touchLetter = document.getElementById("touch-letter");
+var openLetter = document.getElementById("open-letter");
 
 
 /* Initialisation ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+// Login
+loginBtn.onclick = function() { Hide_Login(); }
 
 // Email
 emailIcon.onclick = function() { Open_Email(); }
@@ -40,25 +51,48 @@ envelope.onclick = function() { Show_Letter(); }
 for(i = 0; i < backBtns.length; i++) {
 	// Email Wrapper
 	if (backBtns[i].id == "email-back") {
-		backBtns[i].onclick = function() { Hide_EmailWindow(); }
+		backBtns[i].onclick = function() { Hide_EmailWindow(true); }
 	}
 	// Letter
-	if (backBtns[i].id == "letter-back") {
+	else if(backBtns[i].id == "letter-back") {
 		backBtns[i].onclick = function() { Hide_Letter(); }
 	}
+
+	backBtns[i].onmouseover = function() { Play_Pop(); }
 }
 
 // Audio
+
+Adjust_AudioVolume();
+
 for(i = 0; i < soundBtns.length; i++) {
+
 	soundBtns[i].onmouseover = function() { Play_Pop(); }
 
-	if(soundBtns[i].id != "envelope") {
+	if(soundBtns[i].id == "trash") {
+		soundBtns[i].onmouseover = function() { Play_Pop(); }
+		soundBtns[i].onclick = function() { Play_Pop_PitchedDown(); }
+	}
+	else if(soundBtns[i].id == "envelope") {
+		soundBtns[i].onmouseover = function() { Play_TouchLetter(); }
+	}
+	else if(soundBtns[i].id != "envelope") {
 		soundBtns[i].onclick = function() { Play_PopLoud(); }
 	}
 }
 
 
-/* Functions ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+/* Login ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+
+function Hide_Login() {
+	loginWrapper.style.display = "none";
+	Play_PopLoud();
+	Play_ForestAmbience();
+}
+
+
+/* Email ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
 
 
 function Open_Email() {
@@ -71,7 +105,7 @@ function Show_Message(topic) {
 	for(let i = 0; i < messages.length; i++) {
 
 		if(messages[i].id == topic) {
-			messages[i].style.display = "block";
+			messages[i].style.display = "flex";
 
 		} else {
 			messages[i].style.display = "none";
@@ -81,20 +115,50 @@ function Show_Message(topic) {
 }
 
 
+function Hide_Messages() {
+	for(i = 0; i < messages.length; i++) {
+		messages[i].style.display = "none";
+	}
+}
+
+
+function Hide_EmailWindow(playPop) {
+
+	emailWrapper.style.display = "none";
+
+	Hide_Messages();
+
+	if(!playPop) {
+		return;
+	}
+
+	Play_Pop_PitchedDown();
+}
+
+
+/* Letter ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+
 function Show_Letter() {
 	letterWrapper.style.display = "flex";
-	Hide_EmailWindow();
-	Play_PopLoud();
+	Hide_EmailWindow(false);
+	Play_OpenLetter();
 }
 
 
 function Hide_Letter() {
 	letterWrapper.style.display = "none";
+	Play_Pop_PitchedDown();
 }
 
 
-function Hide_EmailWindow() {
-	emailWrapper.style.display = "none";	
+/* Audio ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+
+function Adjust_AudioVolume() {
+	pop.volume = 0.5;
+	popPitchedDown.volume = 0.75;
+	forestAmbience.volume = 0.125;
 }
 
 
@@ -105,4 +169,22 @@ function Play_Pop() {
 
 function Play_PopLoud() {
 	popLoud.play();
+}
+
+
+function Play_Pop_PitchedDown() {
+	popPitchedDown.play();
+}
+
+
+function Play_ForestAmbience() {
+	forestAmbience.play();
+}
+
+function Play_TouchLetter() {
+	touchLetter.play();
+}
+
+function Play_OpenLetter() {
+	openLetter.play();
 }
