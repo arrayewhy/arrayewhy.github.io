ce7ce8801c4ef434d9070a7e0353990aa973b435
Sundrop <random90s@gmail.com>
2025-11-14 20:12:49 +0800

    Testing new Portfolio Site

---
+++ late-2025/js/script.js
@@ -0,0 +1,34 @@
+const boxes = document.querySelectorAll(".box");
+const close_up_container = document.querySelector("#close-up-container");
+const close_btn = close_up_container.firstElementChild;
+const close_up_img_holder = document.querySelector("#close-up-img-holder");
+
+const img_sources = {
+	"tea-cup":"late-2025/media/images/warm-tea-cup-high-res-cropped.png",
+	"kettle":"late-2025/media/images/cozy-kettle-cropped.png"
+}
+
+_Assign_Functions();
+
+/* Functions ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+function _Assign_Functions() {
+	boxes.forEach(
+			function(e) {
+				e.onclick = function() { _Show_Close_Up(e.id); }
+			}
+		);
+
+	close_btn.onclick = function() { _Hide_Close_Up(); }
+}
+
+/* Close Up ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
+function _Show_Close_Up(id) {
+	close_up_img_holder.src = img_sources[id];
+	close_up_container.style.display = "flex";
+}
+
+function _Hide_Close_Up() {
+	close_up_container.style.display = "none";
+}
