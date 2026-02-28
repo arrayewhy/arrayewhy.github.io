0bfdf6a3609c2ffa8018fd4dbab2e9d357cfe631
Sundrop <random90s@gmail.com>
2026-03-01 01:38:01 +0800

    Degemon: Trying to fix fullscreen

--- media/games/degemon/index.service.worker.js
+++ media/games/degemon/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772174461|80271305';
+const CACHE_VERSION = '1772300262|160132358';
 /** @type {string} */
 const CACHE_PREFIX = 'Degemon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
