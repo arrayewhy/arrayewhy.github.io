afd9c9bcfce26f6c6476c978b0c463a6c1b14605
Sundrop <random90s@gmail.com>
2026-04-24 13:33:15 +0800

    Trying out custom export html.

--- media/games/degemon/index.service.worker.js
+++ media/games/degemon/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773398605|662403831';
+const CACHE_VERSION = '1777008735|158559806';
 /** @type {string} */
 const CACHE_PREFIX = 'Degemon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
